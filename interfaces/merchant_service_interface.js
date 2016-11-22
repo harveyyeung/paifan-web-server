dfbccab1bca3eb2aeebe8bc1dbdb35e2453fe19d
xiex <zhihun.dao@gmail.com>
2016-11-22 17:08:14 +0800

    Draft version of audit merchant/media info.

---
+++ interfaces/merchant_service_interface.js
@@ -0,0 +1,74 @@
+var bluebird = require('bluebird');
+var request = bluebird.promisifyAll(require('request'), {multiArgs: true}); // See: http://stackoverflow.com/questions/34796172/request-getasync-only-returns-1-parameters
+var current_env = process.env.NODE_ENV || "development";
+var config = require('../config/config.json');
+var timeout = config['product-configuration']['httpTimeout'];
+var baseUrl = config['service-connection'][current_env]['merchant-service'];
+var urls = require('../config/url.json')['merchant-service'];
+
+var parseResponseMessage = function (body) {
+    var obj = JSON.parse(body);
+
+    if (obj.type !== undefined && obj.type === "Error") {
+        throw new Error(obj.message);
+    }
+
+    return obj;
+};
+
+var translateStatusString = function (status) {
+    if (!status || status == 'p')
+        return '待审核';
+    else if (status == 'r')
+        return '退回修改';
+    else if (status == 'a')
+        return '审核通过';
+    else
+        return '未知';
+};
+
+exports.requestMediaAuditList = function (auditStatus) {
+    if (!auditStatus)
+        auditStatus = 'all';
+
+    var url = urls['getMediaAuditList'].replace(':auditStatus', auditStatus);
+
+    return request.getAsync({
+        url:  url,
+        baseUrl: baseUrl,
+        timeout: timeout
+    }).spread((res, body) => {
+        var obj = parseResponseMessage(body);   
+
+        if (!obj || !obj.mediaList) {
+            return [];
+        }
+
+        obj.mediaList.forEach(info => info.statusString = translateStatusString(info.mediaInfoStatus));
+
+        return obj.mediaList;
+    });
+};
+
+exports.requestMerchantAuditList = function (auditStatus) {
+    if (!auditStatus)
+        auditStatus = 'all';
+
+    var url = urls['getMerchantAuditList'].replace(':auditStatus', auditStatus);
+
+    return request.getAsync({
+        url:  url,
+        baseUrl: baseUrl,
+        timeout: timeout
+    }).spread((res, body) => {
+        var obj = parseResponseMessage(body);   
+
+        if (!obj || !obj.merchantList) {
+            return [];
+        }
+
+        obj.merchantList.forEach(info => info.statusString = translateStatusString(info.merchantInfoStatus));
+
+        return obj.merchantList;
+    });
+};
--- routes/manage.js
+++ routes/manage.js
@@ -1,7 +1,10 @@
 var express = require('express');
 var router = express.Router();
 var articleServiceInterface = require('../interfaces/article_service_interface');
+var merchantServiceInterface = require('../interfaces/merchant_service_interface');
 var Q = require('q');
+var config = require('../config/config.json');
+var current_env = process.env.NODE_ENV || "development";
 
 router.get('/app_tags/:token', function(req, res, next) {
     Q.all([articleServiceInterface.requestHomeCategories(), articleServiceInterface.requestClassifyTypes()])
@@ -9,6 +12,34 @@ router.get('/app_tags/:token', function(req, res, next) {
         res.render('manage/app_tags', {
             homeCategories: JSON.stringify(home),
             classifyTypes: JSON.stringify(types)
+        });
+    }).catch(err => {
+        return next(err);
+    });
+});
+
+router.get('/audit/media/list', function(req, res, next) {
+    return merchantServiceInterface.requestMediaAuditList().then(result => {
+        res.render('manage/audit_media.ejs', {
+            list: result,
+            rawParameters: {
+                baseUrl: config['base-url'][current_env],
+                merchantServiceUrl: config['extranet-service-connection'][current_env]['merchant-service']
+            }
+        });
+    }).catch(err => {
+        return next(err);
+    });
+});
+
+router.get('/audit/merchant/list', function(req, res, next) {
+    return merchantServiceInterface.requestMerchantAuditList().then(result => {
+        res.render('manage/audit_merchant.ejs', {
+            list: result,
+            rawParameters: {
+                baseUrl: config['base-url'][current_env],
+                merchantServiceUrl: config['extranet-service-connection'][current_env]['merchant-service']
+            }
         });
     }).catch(err => {
         return next(err);
