55be164885670ebdaeb4d9fd22e4701f2b76bac2
xiex <zhihun.dao@gmail.com>
2016-12-08 16:12:32 +0800

    Removed interface to Qiniu.

---
+++ interfaces/shabi-service-interface.js
@@ -0,0 +1,21 @@
+var bluebird = require('bluebird');
+var request = bluebird.promisifyAll(require('request'), {multiArgs: true}); // See: http://stackoverflow.com/questions/34796172/request-getasync-only-returns-1-parameters
+var current_env = process.env.NODE_ENV || "development";
+var config = require('../config/config.json');
+var timeout = config['product-configuration']['httpTimeout'];
+var baseUrl = config['service-connection'][current_env]['merchant-service'];
+var urls = require('../config/url.json')['merchant-service'];
+
+var parseResponseMessage = function (body) {
+    var obj = JSON.parse(body);
+
+    if (obj.type !== undefined && obj.type === "Error") {
+        throw new Error(obj.message);
+    }
+
+    return obj;
+};
+
+exports.uploadImageToQiniu = function (imageBase64) {
+    
+};
